c132118f71405114b2914d5056923466266949ce
David Dixon <david.dixon@amd.com>
2025-07-24 02:53:12 +0000

    Fix tox test integration with rocisa

--- projects/hipblaslt/cmake/hipblaslt_python.cmake
+++ projects/hipblaslt/cmake/hipblaslt_python.cmake
@@ -1,25 +1,5 @@
-# ########################################################################
-# Copyright (C) 2025 Advanced Micro Devices, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
-# ########################################################################
+# Copyright Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier:  MIT
 
 # Sets the HIPBLASLT_PYTHON_COMMAND variable in the parent scope such that it
 # can invoke the Python interpreter valid for the build parameters. Because
@@ -62,5 +42,4 @@ function(hipblaslt_configure_bundled_python_command python_binary_dir asan_optio
     )
     message(VERBOSE "Python command: ${_python_command}")
     set(HIPBLASLT_PYTHON_COMMAND "${_python_command}" PARENT_SCOPE)
-
 endfunction()
