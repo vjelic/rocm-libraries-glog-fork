2a0fb7ee711d4cd7319d0e41e6cc9a131cbc23f8
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-07-28 12:19:36 -0600

    fix: restore install.sh

--- projects/hipsparselt/install.sh
+++ projects/hipsparselt/install.sh
@@ -706,7 +706,6 @@ pushd .
 
   # Build library with AMD toolchain because of existense of device kernels
   if [[ "${build_relocatable}" == true ]]; then
-    echo "!!!!!!!!!!!!!!!!!!!!!! ${compiler} ${cmake_common_options} ${cmake_client_options} !!!!!!!!!!!!!!!!!!!!"
     FC=gfortran CXX=${compiler} CC=${compiler_c} ${cmake_executable} ${cmake_common_options} ${cmake_client_options} -DCPACK_SET_DESTDIR=OFF \
       -DCMAKE_INSTALL_PREFIX=${install_prefix} \
       -DCPACK_PACKAGING_INSTALL_PREFIX=${rocm_path} \
@@ -716,7 +715,6 @@ pushd .
       -DROCM_DISABLE_LDCONFIG=ON \
       -DROCM_PATH="${rocm_path}" ../..
   else
-    echo "!!!!!!!!!!!!!!!!!!!!!! ${compiler} ${cmake_common_options} ${cmake_client_options} !!!!!!!!!!!!!!!!!!!!"
     FC=gfortran CXX=${compiler} CC=${compiler_c} ${cmake_executable} ${cmake_common_options} ${cmake_client_options} -DCPACK_SET_DESTDIR=OFF -DCMAKE_INSTALL_PREFIX=${install_prefix} -DCPACK_PACKAGING_INSTALL_PREFIX=${rocm_path} -DROCM_PATH="${rocm_path}" ../..
   fi
   check_exit_code "$?"
